c706b29be44690aa304ff8fbbd36e522dcabf4f7
Eugene Scherba <escherba@gmail.com>
2013-06-28 12:08:45 -0700

    format previous (buggy) merge-sort implementation for record

--- src/merge-sort.js
+++ src/merge-sort.js
@@ -5,30 +5,31 @@
     * @param {Array.<number>} items Array of items to be sorted.
     */
     aij.mergeSort = function(items) {
+
         /*
         var merge = function(left, right) {
-        var result = [];
+            var result = [];
 
-        while (left.length || right.length) {
-        result.push(
-        left.length && left[0] <= (right[0] || Number.MAX_VALUE) ?
-        left.shift() :
-        right.shift());
-        }
+            while (left.length || right.length) {
+                result.push(
+                    left.length && left[0] <= (right[0] || Number.MAX_VALUE) ?
+                    left.shift() :
+                    right.shift());
+            }
 
-        return result;
+            return result;
         };
 
         var sort = function(arr) {
-        var middle = arr && (arr.length / 2) << 0;
+            var middle = arr && (arr.length / 2) << 0;
 
-        if (!middle) {
-        return arr;
-        } else if (arr.length === 2) {
-        return arr[1] < arr[0] ? arr.reverse() : arr;
-        }
+            if (!middle) {
+                return arr;
+            } else if (arr.length === 2) {
+                return arr[1] < arr[0] ? arr.reverse() : arr;
+            }
 
-        return merge(sort(arr.slice(0, middle)), sort(arr.slice(middle)));
+            return merge(sort(arr.slice(0, middle)), sort(arr.slice(middle)));
         };*/
 
 
@@ -58,4 +59,4 @@
 
     };
 
-})();
+    })();
