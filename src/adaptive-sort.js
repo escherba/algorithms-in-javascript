5f2f5215dab73932ee8c7fe6658ac92bde3e5825
Eugene Scherba <escherba@gmail.com>
2013-07-02 12:12:53 -0700

    free temporary array at once instead of step-by-step

--- src/adaptive-sort.js
+++ src/adaptive-sort.js
@@ -146,25 +146,9 @@
                 }
             }
 
-            /*
             // Step 2: merge everything
-            for (var lim = j - 2; lim >= 0; lim = j - 1) {
-                // At this point, lim == arr.length - 2, so arr[k + 1]
-                // is always defined for any k in [0, lim)
-                for (j = 0, k = 0; k < lim; k = ++j << 1) {
-                    temp[j] = merge(temp[k], temp[k + 1]);
-                }
-                // Last pair is special -- its treatment depends on the initial 
-                // parity of j, which is the same as the current parity of lim).
-                temp[j] = (k > lim) ? temp[k] : merge(temp[k], temp[k + 1]);
-            }
-            var result = temp.shift();
-            temp.length = 0;
-            return result;
-            */
-
-            // Step 2: merge everything
-            for (var j = temp.length; j > 1; temp.length = j) {
+            //for (var j = temp.length; j > 1; temp.length = j) {
+            for (var j = temp.length; j > 1; ) {
                 var lim = j - 2;
                 // At this point, lim == arr.length - 2, so arr[k + 1]
                 // is always defined for any k in [0, lim)
@@ -176,7 +160,7 @@
                 temp[j++] = (k > lim) ? temp[k] : merge(temp[k], temp[k + 1]);
             }
             var result = temp.shift();
-            //temp.length = 0;
+            temp.length = 0;
             return result;
 
 
