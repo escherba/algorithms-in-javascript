b82ad6b2148b5b2b762bb349be2381ccaf191d8e
Eugene Scherba <escherba@gmail.com>
2013-07-14 01:33:59 -0700

    modify flashsort

--- src/flashsort.js
+++ src/flashsort.js
@@ -6,34 +6,34 @@ aij.flashSort = (function(){
 
         var i = 0, j = 0, k = 0, t;
         var m = ~~( n * 0.125 );
-        var l = [];
-        var anmin = a[ 0 ];
+        var a_nmin = a[ 0 ];
         var nmax = 0;
         var nmove = 0;
 
+        var l = new Array(m);
         for ( i = 0; i < m; i++ ) {
             l[ i ] = 0;
         }
 
         for ( i = 1; i < n; ++i ) {
-            if ( a[ i ] < anmin ) { anmin = a[ i ]; }
-            if ( a[ i ] > a[ nmax ] ) { nmax = i; }
+            var a_i = a[ i ];
+            if ( a_i < a_nmin ) { a_nmin = a_i; }
+            if ( a_i > a[ nmax ] ) { nmax = i; }
         }
 
-        if ( anmin === a[ nmax ]) { return a; }
-
-        var c1 = ( m - 1 ) / ( a[ nmax ] - anmin );
+        var a_nmax = a[ nmax ];
+        if ( a_nmin === a_nmax) { return a; }
+        var c1 = ( m - 1 ) / ( a_nmax - a_nmin );
 
         for ( i = 0; i < n; ++i ) {
-            k = ~~( c1 * ( a[ i ] - anmin ) );
-            ++l[ k ];
+            ++l[ ~~( c1 * ( a[ i ] - a_nmin ) ) ];
         }
 
         for ( k = 1; k < m; ++k ) {
             l[ k ] += l[ k - 1 ];
         }
 
-        var hold = a[ nmax ];
+        var hold = a_nmax;
         a[ nmax ] = a[ 0 ];
         a[ 0 ] = hold;
 
@@ -44,7 +44,7 @@ aij.flashSort = (function(){
 
         while ( nmove < i ) {
             while ( j > ( l[ k ] - 1 ) ) {
-                k = ~~( c1 * ( a[ ++j ] - anmin ) );
+                k = ~~( c1 * ( a[ ++j ] - a_nmin ) );
             }
             // line below added 07/03/2013, ES
             if (k < 0) { break; }
@@ -52,11 +52,10 @@ aij.flashSort = (function(){
             flash = a[ j ];
 
             while ( j !== l[ k ] ) {
-                k = ~~( c1 * ( flash - anmin ) );
-                hold = a[ ( t = l[ k ]-1) ];
+                k = ~~( c1 * ( flash - a_nmin ) );
+                hold = a[ t = --l[ k ] ];
                 a[ t ] = flash;
                 flash = hold;
-                --l[ k ];
                 ++nmove;
             }
         }
