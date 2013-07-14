36b593b2b4e9b2143b296ba07432bfb99aec7f1c
Eugene Scherba <escherba@gmail.com>
2013-07-13 19:30:17 -0700

    add non-recursive quick sort implementation

---
+++ src/quickNR-sort.js
@@ -0,0 +1,72 @@
+(function(){
+    /*jshint bitwise: false*/
+    "use strict";
+
+    /**
+    * Quicksort algorithm. It's with complexity O(n log(n)).
+    * In this version of quicksort I use the middle element of the
+    * array for pivot.
+    */
+
+
+    /**
+    * Quicksort algorithm
+    *
+    * @public
+    * @param {array} array Array which should be sorted.
+    * @return {array} Sorted array.
+    */
+    aij.quicknrSort = function (items) {
+
+
+        /**
+        * Partitions the array in two parts by the middle elements.
+        * All elemnts which are less than the chosen one goes left from it
+        * all which are greater goes right from it.
+        *
+        * @param {array} array Array which should be partitioned
+        * @param {number} left Left part of the array
+        * @param {number} right Right part of the array
+        * @return {number}
+        */
+        function partition(array, left, right) {
+            var pivot = array[(left + right) >> 1];
+            while (left <= right) {
+                while (array[left] < pivot) { left++; }
+                while (array[right] > pivot) { right--; }
+                if (left <= right) {
+                    var temp = array[left];
+                    array[left++] = array[right];
+                    array[right--] = temp;
+                }
+            }
+            return left;
+        }
+
+        /**
+        * Iterative implementation of quicksort
+        *
+        * @param {array} array Array which should be processed
+        */
+        function sort(array) {
+            var stack = [ array.length - 1, 0 ];
+            while (stack.length > 0) {
+                var left = stack.pop(),
+                right = stack.pop(),
+                mid = partition(array, left, right);
+                if (left < mid - 1) {
+                    stack.push(mid - 1); // push right
+                    stack.push(left);    // push left
+                }
+                if (right > mid) {
+                    stack.push(right); // push right
+                    stack.push(mid);   // push left
+                }
+            }
+            return array;
+        }
+
+        return sort(items);
+    };
+
+}());
